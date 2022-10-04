300a8c27572d9c544ee20ada3a0c982eeda90d0a
Gacheruian <ian.gacheru@student.moringaschool.com>
2022-10-04 23:54:56 +0300

    add more books to collection

--- src/components/Book.js
+++ src/components/Book.js
@@ -8,7 +8,7 @@ function Book({ img }) {
         <h2>{img.title}</h2>
       </div>
       <div className="book-Image">
-        <img src={img.image} alt="images" width={200}></img>
+        <img src={img.image} alt="images" width={300}></img>
       </div>
       <div className="book-Description">
         <p>{img.Description}</p>
