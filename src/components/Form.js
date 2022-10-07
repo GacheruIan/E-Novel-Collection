cf3cb2b2f92befd28031439cdfb2c3cd696d83f2
Gacheruian <ian.gacheru@student.moringaschool.com>
2022-10-07 09:18:49 +0300

    modify files

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,18 +4,25 @@ function Form() {
   const [title, setTitle] = useState('');
   const [image, setImage] = useState('');
   const [Description, setDescription] = useState('');
-  // const [isPending, setIsPending] = useState('false')
+
 
   function handleSubmit(e){
     e.preventDefault();
     const form = {title,image,Description}
+   
     fetch('http://localhost:3000/images',{
       method: 'POST',
       headers:{'Content-Type':'application/json'},
       body: JSON.stringify(form)
-    }).then(() => {
-      console.log('new book added')
     })
+    .then((res) => res.json())
+    .then(()=>{
+      setTitle('')
+      setImage('')
+      setDescription('')
+  
+    })
+
   }
   return (
     <div>
@@ -27,21 +34,23 @@ function Form() {
         <form className="form-review" onSubmit={handleSubmit} >
           <label for="novel-title">Book-Title</label>
           <br />
-          <input type="text" value={title} onChange={(e)=>setTitle(e.target.value)}></input>
+          <input type="text" value={title} onChange={(e)=>setTitle(e.target.value)} required></input>
           <br />
           <br />
           <label for="image-url">Image url</label> <br />
-          <input type="text" value={image}  onChange={(e)=>setImage(e.target.value)}></input>
+          <input type="text" value={image}  onChange={(e)=>setImage(e.target.value)} required></input>
           <br />
           <br />
           <label for="book-description">Book-Description</label>
           <br />
           <br />
-         <textarea rows = "5" cols = "50"  value={Description} onChange={(e)=>setDescription(e.target.value)}>
+         <textarea rows = "5" cols = "50"  value={Description} onChange={(e)=>setDescription(e.target.value)} required>
             Enter description here...
          </textarea> <br />
-         <br /><button>Add book...</button>
-        </form>
+         <br/>
+          <button>Add book...</button> 
+        
+         </form>
   
       </div>
     </div>
